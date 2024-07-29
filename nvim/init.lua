68a09f44744636f7aa436840b32689594982ce81
3636382e35 <jtuyay10@gmail.com>
2024-07-29 17:38:17 +0800

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,7 @@ require("notify").setup({
 })
 
 function Transparent(color)
-  -- vim.cmd.colorscheme(color)
+  vim.cmd.colorscheme(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -5,9 +5,8 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
+    vim.g.gruvbox_material_enable_italic = false
     vim.g.gruvbox_material_transparent_background = true
     vim.g.gruvbox_material_background = "hard"
-    vim.cmd.colorscheme("gruvbox-material")
   end,
 }
